f02e7272a2e38ffcab131e71a6215b27a2de4386
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-24 19:35:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(mark){
+    return function(obj){
+        return `You are ${mark}${obj}${mark}!`
+    }
+}
